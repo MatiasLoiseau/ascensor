462494a81fe7ca8fcd3b515cd006ddd02c683201
Gastón Zapata <gdz606@hotmail.com>
2017-04-15 15:09:46 -0300

    Update programa.c
    Hola, estuve modificando el ejemplo del Semáforo con estados y puse los 10 estados del ascensor con los tiempos de cambio de estados. Ahora hay que analizar que iría en cada "case". Entiendo que este programa solo actualiza los estados (cambia los estados) y llama a las funciones que serian los bloques o módulos.

--- app/src/programa.c
+++ app/src/programa.c
@@ -1,71 +1,31 @@
-/* Copyright (c) 2017, Eric Pernia.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * =============================================================================
- *
- * Copyright (c) 2017, Eric Pernia.
- * Todos los derechos reservados.
- * 
- * La redistribución y el uso en las formas de código fuente y binario, con o 
- * sin modificaciones, están permitidos siempre que se cumplan las siguientes 
- * condiciones:
- * 
- * 1. Las redistribuciones del código fuente deben conservar el aviso de
- *    copyright anterior, esta lista de condiciones y el siguiente descargo de
- *    responsabilidad.
- * 
- * 2. Las redistribuciones en formato binario deben reproducir el aviso de
- *    copyright anterior, esta lista de condiciones y el siguiente descargo de
- *    responsabilidad en la documentación y/u otros materiales suministrados 
- *    con la distribución.
- * 
- * 3. Ni el nombre de los titulares de derechos de autor ni los nombres de sus
- *    colaboradores pueden usarse para apoyar o promocionar productos 
- *    derivados de este software sin permiso específico previo y por escrito.
- * 
- * ESTE SOFTWARE SE SUMINISTRA POR LOS PROPIETARIOS DEL COPYRIGHT Y
- * COLABORADORES ''COMO ESTÁ'' Y CUALQUIER GARANTÍAS EXPRESA O IMPLÍCITA,
- * INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS IMPLÍCITAS DE COMERCIALIZACIÓN
- * Y APTITUD PARA UN PROPÓSITO PARTICULAR SON RECHAZADAS. EN NINGÚN CASO LOS
- * PROPIETARIOS DEL COPYRIGHT Y COLABORADORES SERÁN RESPONSABLES POR NINGÚN
- * DAÑO DIRECTO, INDIRECTO, INCIDENTAL, ESPECIAL, EJEMPLAR O COSECUENCIAL
- * (INCLUYENDO, PERO NO LIMITADO A, LA ADQUISICIÓN O SUSTITUCIÓN DE BIENES O
- * SERVICIOS; LA PÉRDIDA DE USO, DE DATOS O DE BENEFICIOS; O INTERRUPCIÓN DE LA
- * ACTIVIDAD EMPRESARIAL) O POR CUALQUIER TEORÍA DE RESPONSABILIDAD, YA SEA POR
- * CONTRATO, RESPONSABILIDAD ESTRICTA O AGRAVIO (INCLUYENDO NEGLIGENCIA O
- * CUALQUIER OTRA CAUSA) QUE SURJA DE CUALQUIER MANERA DEL USO DE ESTE SOFTWARE,
- * INCLUSO SI SE HA ADVERTIDO DE LA POSIBILIDAD DE TALES DAÑOS.
- *
- * =============================================================================
- * Fecha de creacion: 2017-03-15
- * =============================================================================
- */
+/*============================================================================
+ * Autor: Grupo 4
+ * Fecha: 2017-14-04
+ 
+Entregable 10: Ascensor
+   Estados:
+      - 1-EN_ESPERA
+      - 2-SUBIENDO
+      - 3-BAJANDO
+      - 4-ABRIENDO_PUERTA
+	  - 5-PUERTA_ABIERTA
+	  - 6-CERRANDO_PUERTA
+	  - 7-PUERTA_CERRADA
+	  - 8-MODO_CONFIGURACION
+	  - 9-LEER_PISO
+	  - 10-ALARMA_PUERTA_ABIERTA
+	  
+   Cambio de estados por tiempo:
+      
+	  -SUBIENDO: 1seg  //tiempo que tarda en subir un piso
+	  -BAJANDO: 1seg   //tiempo que tarda en bajar un piso
+	  -ABRIENDO_PUERTA: 1seg
+	  -CERRANDO_PUERTA: 1seg
+	  -PUERTA_ABIERTA: 2 seg
+	  -ALARMA_PUERTA_ABIERTA: 3seg // si pasan 3 seg con puerta abierta activa la alarma
+      
+ 
+ *===========================================================================*/
 
 /*==================[inlcusiones]============================================*/
 
@@ -74,13 +34,42 @@
 
 /*==================[definiciones y macros]==================================*/
 
+// Nuevo tipo de datos enumerado llamado estadoMEF
+typedef enum{
+   EN_ESPERA,          		// 0
+   SUBIENDO,           		// 1
+   BAJANDO,            		// 2
+   ABRIENDO_PUERTA,    		// 3
+   PUERTA_ABIERTA,     		// 4
+   CERRANDO_PUERTA,    		// 5
+   PUERTA_CERRADA,     		// 6
+   MODO_CONFIGURACION,  	// 7
+   LEER_PISO,           	// 8
+   ALARMA_PUERTA_ABIERTA,   //10
+    
+} estadoMEF_t;
+      
+#define SEG_TO_MS(val) ((val)*1000)
+
 /*==================[definiciones de datos internos]=========================*/
 
 /*==================[definiciones de datos externos]=========================*/
 
+/*==================[definiciones de datos globales]=========================*/
+
+// Variable de estado (global)
+estadoMEF_t estadoActual;
+
 /*==================[declaraciones de funciones internas]====================*/
 
 /*==================[declaraciones de funciones externas]====================*/
+
+// Prototipos de funciones
+void InicializarMEF( void );
+void ActualizarMEF( void );
+
+
+void romperMEF( void );
 
 /*==================[funcion principal]======================================*/
 
@@ -89,22 +78,16 @@ int main( void ){
 
    // ---------- CONFIGURACIONES ------------------------------
    // Inicializar y configurar la plataforma
-   boardConfig();   
+   boardConfig();
+  
+   InicializarMEF();
    
    // ---------- REPETIR POR SIEMPRE --------------------------
    while( TRUE )
    {      
-      // Si se presiona TEC1, enciende el LEDB
-      gpioWrite( LEDB, !(gpioRead(TEC1)) );
+      ActualizarMEF();
       
-      // Si se presiona TEC2, enciende el LED1
-      gpioWrite( LED1, !(gpioRead(TEC2)) );
-      
-      // Si se presiona TEC3, enciende el LED2
-      gpioWrite( LED2, !(gpioRead(TEC3)) );
-      
-      // Si se presiona TEC4, enciende el LED3
-      gpioWrite( LED3, !(gpioRead(TEC4)) );
+      romperMEF();
    } 
 
    // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta 
@@ -116,5 +99,85 @@ int main( void ){
 /*==================[definiciones de funciones internas]=====================*/
 
 /*==================[definiciones de funciones externas]=====================*/
+
+void romperMEF( void ){
+   if( !gpioRead(TEC4) ){
+      estadoActual = 58;      
+   }
+}
+
+      
+
+delay_t tiempoSubiendo;
+delay_t tiempoBajando;
+delay_t tiempoAbriendoPuerta;
+delay_t tiempoCerrandoPuerta;
+delay_t tiempoPuertaAbierta;
+delay_t tiempoAlarmaPuertaAbierta;
+
+
+// Función Inicializar MEF
+void InicializarMEF( void ){
+   estadoActual = EN_ESPERA;
+   
+   delayConfig( &tiempoSubiendo, SEG_TO_MS(1) );
+   delayConfig( &tiempoBajando, SEG_TO_MS(1) );
+   delayConfig( &tiempoAbriendoPuerta, SEG_TO_MS(1) );
+   delayConfig( &tiempoCerrandoPuerta, SEG_TO_MS(1) );  
+   delayConfig( &tiempoPuertaAbierta, SEG_TO_MS(2) );
+   delayConfig( &tiempoAlarmaPuertaAbierta, SEG_TO_MS(3) );   
+}
+
+// Función Actualizar MEF
+void ActualizarMEF(void){
+   
+   switch(estadoActual) {
+      
+      case EN_ESPERA:
+               
+      break;
+      
+      case SUBIENDO:
+                
+      break;
+      
+      case BAJANDO:
+         
+      break;
+      
+      case ABRIENDO_PUERTA:
+           
+      break;
+	  
+	  case CERRANDO_PUERTA:
+         
+	  break;
+		 
+	  case PUERTA_CERRADA:
+         
+	  break; 
+	  
+	  case MODO_CONFIGURACION:
+        
+	  break;
+		 
+	  case LEER_PISO:
+         
+	  break;
+		 
+	  case ALARMA_PUERTA_ABIERTA:
+         
+      break;
+      
+      default:
+         //Si algo modificó la variable estadoActual
+         // a un estado no válido llevo la MEF a un
+         // lugar seguro, por ejemplo, la reinicio:
+         InicializarMEF();
+      break;
+         
+   }      
+}
+
 
 /*==================[fin del archivo]========================================*/
