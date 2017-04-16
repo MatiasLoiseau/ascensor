3b0454afdd22cd8101aae5efa69b3baefc783ed8
Matias Loiseau <matiasloiseau@gmail.com>
2017-04-16 20:19:17 -0300

    Add files via upload

---
+++ app/src/driverDisplays7Segmentos.c
@@ -0,0 +1,32 @@
+/*============================================================================
+ * Autor: Grupo 4
+ * Fecha: 2017-14-04 
+ *===========================================================================*/
+
+/*==================[inlcusiones]============================================*/
+
+#include "programa.h"   // <= su propio archivo de cabecera
+#include "sapi.h"       // <= Biblioteca sAPI
+#include "driverDisplays7Segmentos.h"
+#include "driverTecladoMatricial.h"
+#include "mefAscensor.h"
+#include "mefIngresoPiso.h"
+#include "mefModoConfiguracion.h"
+#include "mefPuertas.h"
+/*==================[definiciones y macros]==================================*/
+
+/*==================[definiciones de datos internos]=========================*/
+
+/*==================[definiciones de datos externos]=========================*/
+
+/*==================[definiciones de datos globales]=========================*/
+
+/*==================[declaraciones de funciones internas]====================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+/*==================[definiciones de funciones internas]=====================*/
+
+/*==================[definiciones de funciones externas]=====================*/
+
+/*==================[fin del archivo]========================================*/
