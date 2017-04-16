43b810b421067b28b7110f5713344f78d2612765
Matias Loiseau <matiasloiseau@gmail.com>
2017-04-16 20:13:02 -0300

    Add files via upload

---
+++ app/inc/driverTecladoMatricial.h
@@ -0,0 +1,26 @@
+#ifndef _DRIVERTECLADOMATRICIAL_H_
+#define _DRIVERTECLADOMATRICIAL_H_
+
+/*==================[inclusiones]============================================*/
+
+/*==================[c++]====================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[tipos de datos declarados por el usuario]===============*/
+
+/*==================[declaraciones de datos externos]========================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+/*==================[c++]====================================================*/
+#ifdef __cplusplus
+}
+#endif
+
+/*==================[end of file]============================================*/
+#endif /* _DRIVERTECLADOMATRICIAL_H_ */
